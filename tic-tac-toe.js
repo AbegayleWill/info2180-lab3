d51c97634520e39a6f364a8986ec614a5b9b1224
Abegayle Williams <abewilli10@gmail.com>
2021-10-09 20:31:41 -0500

    Edited javascript to allow adding of X or O to square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,35 @@
 window.onload = function(){windowLoad()}
-function windowLoad(){
-    var blocks = document.getElementById("board").childNodes;
-    blocks.forEach(element => {element.className = "square";});
+    function windowLoad(){
+        var blocks = document.getElementById("board").childNodes;
+        blocks.forEach(element => {element.className = "square";});
+
+
+
+        
+        const xClass= "X"
+        const oClass = "O"
+        let circleTurn 
+
+        const stateOfGame =[]
+        blocks.forEach(element => {element.addEventListener("click", isClicked)})
+        function isClicked(element){
+            const space = element.target
+            const currentClass = circleTurn ? oClass : xClass
+
+            function placePlay(space, currentClass){
+                space.textContent = currentClass
+                space.classList.add(currentClass)
+                space.style.pointerEvents = "none"
+            }
+            placePlay(space,currentClass)
+            stateOfGame.push(currentClass)
+            circleTurn =! circleTurn
+            //console.log("button clicked");
+        }
+
+
+       
+
 }
+
+
