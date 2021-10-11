42fccd65907c8093db9cf3eadcdfc6dcbd0750a2
Abegayle Williams <abewilli10@gmail.com>
2021-10-10 22:50:46 -0500

    updated to check winner (doesn't work very well)

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,28 +5,38 @@ window.onload = function(){windowLoad()}
         blocks.forEach(element => {element.className = "square";});
 
 
-
-        
         const xClass= "X"
         const oClass = "O"
         let circleTurn 
 
-        const stateOfGame =[]
+        let stateOfGame= [];
         blocks.forEach(element => {element.addEventListener("click", isClicked)})
         function isClicked(element){
             const space = element.target
-            const currentClass = circleTurn ? oClass : xClass
+            const currentPlayer = circleTurn ? oClass : xClass
 
-            function placePlay(space, currentClass){
-                space.textContent = currentClass
-                space.classList.add(currentClass)
-                space.style.pointerEvents = "none"
-            }
-            placePlay(space,currentClass)
-            stateOfGame.push(currentClass)
+            placePlay(space,currentPlayer )
+            stateOfGame.push(currentPlayer )
             circleTurn =! circleTurn
             //console.log("button clicked");
+          
+            function placePlay(space, currentPlayer ){
+              space.textContent = currentPlayer 
+              space.classList.add(currentPlayer)
+              space.style.pointerEvents = "none"
+          }
+
+          playerWon()
+          /*if (playerWon(currentPlayer)) {
+            endGame(false)
+          } else if (isDraw()) {
+            endGame(true)
+          } else {
+            swapTurns()
+          }*/
         }
+          
+        
 
         
         blocks.forEach(function(element){
@@ -40,8 +50,83 @@ window.onload = function(){windowLoad()}
                 hoverPlay.target.classList.remove("hover");
             })
         })
-       
+        
+
+        /*function swapTurns() {
+          circleTurn = !circleTurn
+        }*/
+
+        
+        
+        
+        
+       /* const WINNING_COMBINATIONS = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6]
+        ]*/
+        
+
+        
+        //const strategy = document.querySelector('status')
+        //const currentPlayer = circleTurn ? oClass : xClass
+        //const text = document.querySelector('#heading');
+
+        
+
+        const currentPlayer = circleTurn ? oClass : xClass
+
+        const playerWon = () => {
+          let winnerMessage= document.getElementById("status");
+          winnerMessage.textContent= `Congragulations! ${currentPlayer} is the Winner!`
+          winnerMessage.classList += "you-won";
+            if (stateOfGame[0].innerText == currentPlayer) {
+              if (stateOfGame[1].innerText == currentPlayer && stateOfGame[2].innerText == currentPlayer) {
+                winnerMessage
+                return true;
+              }
+              if (stateOfGame[3].innerText == currentPlayer && stateOfGame[6].innerText== currentPlayer) {
+                winnerMessage
+                return true;
+              }
+              if (stateOfGame[4].innerText== currentPlayer && stateOfGame[8].innerText == currentPlayer) {
+                winnerMessage
+                return true;
+              }
+            }
+            if (stateOfGame[8].innerText == currentPlayer) {
+              if (stateOfGame[2].innerText == currentPlayer && stateOfGame[5].innerText == currentPlayer) {
+                console.log(`${currentPlayer} wins`);
+                return true;
+              }
+              if (stateOfGame[6].innerText == currentPlayer &&stateOfGame[7].innerText == currentPlayer) {
+                winnerMessage
+                return true;
+              }
+            }
+            if (stateOfGame[4].innerText == currentPlayer) {
+              if (stateOfGame[1].innerText == currentPlayer && stateOfGame[7].innerText == currentPlayer) {
+                winnerMessage
+                return true;
+              }
+              if (stateOfGame[3].innerText == currentPlayer && stateOfGame[5].innerText == currentPlayer) {
+                winnerMessage
+                return true;
+              }
+              if (stateOfGame[2].innerText == currentPlayer && stateOfGame[6].innerText == currentPlayer) {
+                winnerMessage
+                return true;
+            
+              }
+            }
+          };
+          
+          
+        
 
 }
-
-
